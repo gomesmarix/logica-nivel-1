cecf04c46199be88e32a3a9be8b299def873a0e9
gomesmarix <gomesmariana352@gmail.com>
2025-03-18 21:20:57 -0300

    Create ex-04.py

---
+++ ex-04.py
@@ -0,0 +1,4 @@
+#Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+print ("Mariana")
+print ("Rio de Janeiro")
+print ("Rio de Janeiro") 
