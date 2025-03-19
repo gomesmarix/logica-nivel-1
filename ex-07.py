0a3e73d18c7fbc3a91a14c3111708b10fe732dae
gomesmarix <gomesmariana352@gmail.com>
2025-03-18 21:43:44 -0300

    Create ex-07.py

---
+++ ex-07.py
@@ -0,0 +1,7 @@
+#Você foi contratado por uma escola pra fazer o sistema de boletim dos alunos. Como primeiro passo, escreva um programa que produza a seguinte saída:
+print ("Aluno (A)     Nota")
+print ("========      ====")
+print ("Aline         9.0")
+print ("Mário         DEZ")
+print ("Sérgio        4.5")
+print ("Shirley       7.0")
