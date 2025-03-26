af0e10888d2436d373316ed276489d104279d2dc
gomesmarix <gomesmariana352@gmail.com>
2025-03-25 21:07:29 -0300

    Create ex-09.py

---
+++ ex-09.py
@@ -0,0 +1,11 @@
+#Implemente um programa que desenhe um "pinheiro" na tela, similar ao abaixo.
+print("      X")
+print("     XXX")
+print("    XXXXX")
+print("   XXXXXXX")
+print("  XXXXXXXXX")
+print(" XXXXXXXXXXX")
+print("XXXXXXXXXXXXX")
+print("     XX")
+print("     XX      ")
+print("    XXXX")
